3327388585375c1982a8cebc3bb7eb7f92207785
kscode <kashinggo@gmail.com>
2023-02-17 19:23:07 +0800

    update

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://menglianai-v0k3.onrender.com', {
+    const response = await fetch('http://164.88.150.46:8000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
